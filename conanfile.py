5a61a3d1eeb2d9e1b6838b89b13a709d2693cc00
a_teammate <madoe3@web.de>
2016-11-06 16:10:19 +0100

    add option SSE + install nasm (on win and mac) + fix cmake runtime option

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ import os
 from conans.tools import download
 from conans.tools import unzip, replace_in_file
 from conans import CMake
+import shutil, sys
 
 
 class LibJpegTurboConan(ConanFile):
@@ -11,18 +12,14 @@ class LibJpegTurboConan(ConanFile):
     ZIP_FOLDER_NAME = "%s-%s" % (name, version)
     generators = "cmake", "txt"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
+    options = {"shared": [True, False], "fPIC": [True, False], "SSE": [True, False]}
+    default_options = "shared=False", "fPIC=True", "SSE=True"
     exports = "CMakeLists.txt"
     url="http://github.com/lasote/conan-libjpeg-turbo"
     license="https://github.com/libjpeg-turbo/libjpeg-turbo/blob/1.4.2/LICENSE.txt"
     
     def config(self):
-        try: # Try catch can be removed when conan 0.8 is released
-            del self.settings.compiler.libcxx 
-        except: 
-            pass
-        
+        del self.settings.compiler.libcxx # pure c lib
         if self.settings.os == "Windows":
             self.options.remove("fPIC")
        
@@ -37,6 +34,36 @@ class LibJpegTurboConan(ConanFile):
             to reuse it later in any other project.
         """
         env = ConfigureEnvironment(self.deps_cpp_info, self.settings)
+
+        set_path_command = ""
+        # Download nasm as build tool. This should go to source()
+        if self.options.SSE == True:
+            if self.settings.os == "Linux":
+                # TODO: We should build nasm from source then.
+                self.options.SSE = False # Or is removing here better? I'm not familiar with python..
+            else:
+                nasm_version = "2.12.02"
+                nasm_os_url_id = "" #nasm url identifier
+                if self.settings.os == "Windows":
+                    if self.settings.arch == "x86":
+                        nasm_os_url_id = "win32"
+                    else:
+                        nasm_os_url_id = "win64" 
+                elif self.settings.os == "Macos":
+                    nasm_os_url_id = "macosx"
+                nasm_folder_name = "nasm-%s-%s" % (nasm_version, nasm_os_url_id)
+                nasm_zip_name = "%s.zip" % nasm_folder_name
+                download("http://www.nasm.us/pub/nasm/releasebuilds/%s/%s/%s" % (nasm_version, nasm_os_url_id, nasm_zip_name), nasm_zip_name)
+                self.output.warn("Downloading nasm: http://www.nasm.us/pub/nasm/releasebuilds/%s/%s/%s" % (nasm_version, nasm_os_url_id, nasm_zip_name))
+                unzip(nasm_zip_name)
+                os.unlink(nasm_zip_name)
+                nasm_path = os.path.join(os.getcwd(), nasm_folder_name)
+
+                #env.environ["PATH"] += os.pathsep + nasm_path #its probably as easy as this, but i cant append to the path self.run operates in.
+                if self.settings.os == "Windows":
+                    set_path_command = "set \"PATH=%s\" &&" % os.environ["PATH"]
+                else:
+                    set_path_command = "PATH=\"%s\" &&" % os.environ["PATH"]
 
         if self.settings.os == "Linux" or self.settings.os == "Macos":
             if self.options.fPIC:
@@ -58,7 +85,7 @@ class LibJpegTurboConan(ConanFile):
 
             self.run("cd %s && %s ./configure %s" % (self.ZIP_FOLDER_NAME, env_line, config_options))
             self.run("cd %s && %s make" % (self.ZIP_FOLDER_NAME, env_line))
-        else:
+        else: # We should (for simplicity) always use cmake shouldnt we?
             conan_magic_lines = '''project(libjpeg-turbo)
     cmake_minimum_required(VERSION 3.0)
     include(../conanbuildinfo.cmake)
@@ -68,11 +95,25 @@ class LibJpegTurboConan(ConanFile):
             replace_in_file("%s/CMakeLists.txt" % self.ZIP_FOLDER_NAME, "project(libjpeg-turbo C)", "")
             
             cmake = CMake(self.settings)
-            self.run("cd %s && mkdir _build" % self.ZIP_FOLDER_NAME)
-            cd_build = "cd %s/_build" % self.ZIP_FOLDER_NAME
-            self.run('%s && cmake .. %s -DWITH_CRT_DLL=ON' % (cd_build, cmake.command_line)) # Don't change runtime, conan will take care of
-            self.run("%s && cmake --build . %s" % (cd_build, cmake.build_config))
-                
+            builddir = os.path.join(self.ZIP_FOLDER_NAME, "_build")
+
+            if os.path.exists(builddir):
+                shutil.rmtree(builddir) # We need to remove this folder first for windows
+            os.makedirs(builddir)
+
+            cmake_options = []
+            if self.options.shared == True:
+                cmake_options += ["-DENABLE_STATIC=0"]
+            else:
+                cmake_options = ["-DENABLE_SHARED=0"]
+            cmake_options += ["-DWITH_SIMD=%s" % "1" if self.options.SSE else "0"]
+
+             # why this comment: "Don't change runtime, conan will take care of"? conan_basic_setup() runs before this cmake option replaces MT with MD again
+            cmake_options += ["-DWITH_CRT_DLL=%s" % "1" if self.settings.compiler.runtime == "MD" or self.settings.compiler.runtime == "MDd" else "0"]
+
+            self.run('%s cd %s && cmake .. %s %s' % (set_path_command, builddir, cmake.command_line, " ".join(cmake_options)))
+            self.run("%s cd %s && cmake --build . %s" % (set_path_command, builddir, cmake.build_config))
+
     def package(self):
         """ Define your conan structure: headers, libs, bins and data. After building your
             project, this method is called to create a defined structure:
